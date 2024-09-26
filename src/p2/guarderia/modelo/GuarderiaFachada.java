d39f7d11710dc1d0bcf2df052f8c5dd77de8cb2b
yefre <yefre@BEYMAR>
2024-09-26 00:11:14 -0500

    Guarderia Fachada Métodos Finalizados

--- src/p2/guarderia/modelo/GuarderiaFachada.java
+++ src/p2/guarderia/modelo/GuarderiaFachada.java
@@ -4,12 +4,90 @@
  */
 package p2.guarderia.modelo;
 
+import java.util.ArrayList;
+
 /**
  *
- * @author Boris Perez
+ * @author Beymar Villamizar
  */
 public class GuarderiaFachada {
  
+    private IDAO idao;
     
+    public void agregarMascota(String id, String nombre, String edad, String raza) throws Exception{
+       idao = new MascotaDAOFile();
+       MascotaDTO m = new MascotaDTO();
+       m.setId(id);
+       m.setNombre(nombre);
+       m.setEdad(edad);
+       m.setRaza(raza);
+       idao.agregar(m); // agrego a el csv
+    }
     
+    public void eliminarMascota(String idMascota) throws Exception{
+        idao = new MascotaDAOFile();
+        idao.eliminar(idMascota);
+    }
+    
+    public void actualizarMascota(String id, String nombre, String edad, String raza) throws Exception{
+       idao = new MascotaDAOFile();
+       MascotaDTO m = (MascotaDTO) idao.buscar(id); //Casteo el object que me retorna idao
+       m.setId(id);
+       m.setNombre(nombre);
+       m.setEdad(edad);
+       m.setRaza(raza);
+    }
+    
+    public String[] buscarMascota(String id) throws Exception{
+        idao = new MascotaDAOFile();
+        MascotaDTO m = (MascotaDTO) idao.buscar(id);
+        String [] info = new String[4];
+        if(m!=null){
+            info[0] = m.getId();
+            info[1] = m.getNombre();
+            info[2] = m.getEdad();
+            info[3] = m.getRaza();
+        }
+        return info;
+    }
+    
+    public void asignarPropietarioMascota(String idMascota, String idPersona) throws Exception{
+        idao = new MascotaDAOFile();
+        MascotaDTO m = (MascotaDTO) idao.buscar(idMascota);
+        idao = new PersonaDAOFile();
+        PersonaDTO p = (PersonaDTO) idao.buscar(idPersona);
+        if(m!=null & p!=null){
+            m.setPropietario(p);
+        }
+    }
+    
+    public void retirarPropietarioMascota(String idMascota, String idPersona) throws Exception{
+        idao = new MascotaDAOFile();
+        MascotaDTO m = (MascotaDTO) idao.buscar(idMascota);
+        idao = new PersonaDAOFile();
+        PersonaDTO p = (PersonaDTO) idao.buscar(idPersona);
+        if(m!=null & p!=null){
+            m.setPropietario(null);
+        }
+    }
+    
+    public String[] listarPersonas() throws Exception{
+        idao = new PersonaDAOFile();
+        ArrayList<IObjetoDTO> personas = idao.listar();
+        String [] lista = new String[personas.size()];
+        for(int i=0; i<personas.size(); i++){
+            lista[i] = personas.get(i).toString();
+        }
+        return lista;
+    }
+    
+    public String[] listarMascotas() throws Exception{
+        idao = new MascotaDAOFile();
+        ArrayList<IObjetoDTO> mascotas = idao.listar();
+        String [] lista = new String[mascotas.size()];
+        for(int i=0; i<mascotas.size(); i++){
+            lista[i] = mascotas.get(i).toString();
+        }
+        return lista;
+    }
 }
