3214d55bfd71ef7151f1889762d3cd2054059145
yefre <yefre@BEYMAR>
2024-09-30 23:51:17 -0500

    Métodos eliminar y Agregar en MascotaDaoFile

--- src/p2/guarderia/modelo/MascotaDAOFile.java
+++ src/p2/guarderia/modelo/MascotaDAOFile.java
@@ -4,6 +4,11 @@
  */
 package p2.guarderia.modelo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -14,12 +19,56 @@ public class MascotaDAOFile implements IDAO{
 
     @Override
     public void agregar(IObjetoDTO dto) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet"); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String ruta = "mascotas.csv";
+        MascotaDTO m = (MascotaDTO) dto;
+        
+        // Crear la línea con los datos de la mascota
+        String linea = m.getId()+";"+m.getNombre()+";"+m.getRaza()+";"+m.getEdad()+";";
+        
+        if(m.getPropietario()!=null)
+            linea += m.getPropietario();
+        else
+            linea += "null";
+              
+        try(BufferedWriter file = new BufferedWriter(new FileWriter(ruta))){
+            file.write(linea);
+            file.newLine();
+        }catch(IOException e){
+            System.out.println("Error al agregar la mascota: "+e.getMessage());
+        }
+       
     }
 
     @Override
     public void eliminar(String id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
+        String ruta = "mascotas.csv", separador = ";", linea;
+        ArrayList<String> lineas = new ArrayList<>();
+
+        // Leer el archivo línea por línea y almacenar las que no coincidan con el id a eliminar
+        try (BufferedReader file = new BufferedReader(new FileReader(ruta))) {
+            while ((linea = file.readLine()) != null) { // <-- Mientras halla lineas para leer
+                
+                String[] datos = linea.split(separador); // <-- Guardar los datos de la linea en un arreglo String
+                String idLinea = datos[0]; // <-- Obtener el ID de la mascota
+
+                if (!(idLinea.equals(idLinea))) {
+                    lineas.add(linea); // <-- Agregar linea si el Id a eliminar no es el mismo
+                 }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+
+        // Reescribir el archivo 
+        try (BufferedWriter fileNew = new BufferedWriter(new FileWriter(ruta))) {
+            for (String lineaActual : lineas) {
+                fileNew.write(lineaActual);
+                fileNew.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error al escribir el archivo: " + e.getMessage());
+        }
     }
 
     @Override
@@ -30,6 +79,7 @@ public class MascotaDAOFile implements IDAO{
     @Override
     public IObjetoDTO buscar(String id) throws Exception {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     @Override
